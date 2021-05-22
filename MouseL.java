16bb326342d0bb43c1684c7916af602440b3e550
siddharth chaudhari <75258737+Siddharth-Chaudhari@users.noreply.github.com>
2021-05-22 20:25:34 +0530

    Add files via upload

---
+++ MouseL.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import java.awt.event.*;
+public class MouseL extends Frame implements MouseListener
+{
+	Label l;
+	MouseL()
+	{
+		addMouseListener(this);
+		
+			addMouseListener(this);
+		
+			l=new Label();
+			l.setBounds(90,70,100,200);
+			add(l);
+			setSize(300,300);
+			setLayout(null);
+			setVisible(true);
+	}
+		public  void mouseClicked(MouseEvent e)
+		{
+			l.setText("Mouse Click");
+		} 
+		public  void mouseEntered(MouseEvent e)
+		{
+			l.setText("Mouse Entered");
+		}
+		public  void mouseExited(MouseEvent e)
+		{
+			l.setText("Mouse Exited");
+		}
+		public void mousePressed(MouseEvent e)
+		{
+			l.setText("Mouse Pressed");
+		}
+		public  void mouseReleased(MouseEvent e)
+		{
+			l.setText("Mouse Released");
+		}
+		
+		public static void main(String args[])
+		{
+			new MouseL();
+		}
+	}
